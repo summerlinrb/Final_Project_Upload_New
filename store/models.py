83647fb1b89a9a21de8dbc4d2025064146542bbf
Brandon Summerlin <bsummerlin1@buffs.wtamu.edu>
2021-11-15 21:01:12 -0600

    added URL

--- store/models.py
+++ store/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
-	image = models.ImageField(upload_to="{% static 'images' %}",null=True, blank=True)
+	image = models.ImageField(upload_to="static/images",null=True, blank=True)
 
 	def __str__(self):
 		return self.name
