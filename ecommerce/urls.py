60d605228147b2ff22d4f3c706f93b25182bd1ac
Brandon Summerlin <bsummerlin1@buffs.wtamu.edu>
2021-11-15 21:20:48 -0600

    added URL

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -30,10 +30,13 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
-                              
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+ 
+
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
